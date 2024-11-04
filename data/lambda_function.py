559719a16db7f3b8741d18982e0d260e88acd009
Steve Sajeev <stevesajeev123@gmail.com>
2024-11-04 13:45:17 -0500

    [data] Revert lambda function
    There is no need to save two days' worth of listings as the listings can simply be filtered to only be those made on that specific day.

--- data/lambda_function.py
+++ data/lambda_function.py
@@ -6,19 +6,14 @@ def lambda_handler(event, context):
     # Get job listings from SimplifyJobs repo
     GITHUB_URL = "https://raw.githubusercontent.com/SimplifyJobs/Summer2025-Internships/refs/heads/dev/.github/scripts/listings.json"
     r = requests.get(GITHUB_URL)
+
+    # Upload file to S3 bucket
+    bucket_name = "acm-github-data"
+    file_name = "listings.json"
     file_content = r.content
 
-    # Interact with S3 bucket
     s3 = boto3.client("s3")
-    bucket_name = "acm-github-data"
-    
-    # Update existing data to yesterday's data
-    curr_file_name = "listings.json"
-    prev_file_name = "listings-prev.json"
-    s3.copy_object(Bucket=bucket_name, CopySource=f"{bucket_name}/{curr_file_name}", Key=prev_file_name)
-
-    # Put request's data as current data
-    s3.put_object(Body=file_content, Bucket=bucket_name, Key=curr_file_name)
+    s3.put_object(Body=file_content, Bucket=bucket_name, Key=file_name)
 
     return {
         "statusCode": 200,
