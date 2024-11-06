b4a9098d3de94e814374dca3d0750cbec002448f
Steve Sajeev <stevesajeev123@gmail.com>
2024-11-06 14:52:09 -0500

    [discord] Add status for bot

--- discord/bot.py
+++ discord/bot.py
@@ -8,10 +8,20 @@ load_dotenv()
 
 
 intents = discord.Intents.default()
+intents.guilds = True
+
 bot = commands.Bot(command_prefix="", intents=intents)
 
 
+@bot.event
+async def on_ready():
+    activity = discord.Activity(
+        type=discord.ActivityType.watching, name="for new opportunities!")
+    await bot.change_presence(activity=activity)
+
+
 async def main():
+    await bot.load_extension("setup")
     await bot.load_extension("post_listings")
     await bot.start(os.getenv("BOT_TOKEN"))
 
